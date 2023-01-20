55facf238405f5255a838c465a0d7c43f7bee329
Pavel Belov <job.pb@yandex.ru>
2023-01-20 22:21:30 +0300

    add home work 3 task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1. Найти кубы чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int value = 0;
+int number = 1;
+
+while (number <= N)
+{
+    value = number;
+    value *= value *= value;
+    Console.WriteLine("Куб числа " + number + " равен " + value);
+    number++;
+}
