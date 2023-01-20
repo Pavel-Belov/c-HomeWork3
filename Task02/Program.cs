0e9e2b46504871d12ca8b5ddad63bd9bdfd83601
Pavel Belov <job.pb@yandex.ru>
2023-01-20 22:49:07 +0300

    add home work 3 task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2. Подсчитать сумму цифр в числе
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int num = 0;
+int sum = 0;
+int count = 1;
+
+while (count <= number)
+{
+    num = number % (count * 10) / count;
+    sum += num;
+    count *= 10;
+}
+
+Console.WriteLine("Сумма цифр в числе " + number + " равна " + sum);
