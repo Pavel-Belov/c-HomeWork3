7f54285d5fd07795aa3845f0fa5811df5c1c7881
Pavel Belov <job.pb@yandex.ru>
2023-01-20 22:55:55 +0300

    add home work 3 task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int number = 1;
+int result = 1; 
+
+while (number <= N)
+{
+    result *= number;
+    number++;
+}
+
+Console.WriteLine("Произведение числе от 1 до " + N + " равно " + result);
